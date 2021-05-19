f380760382ae0eb16fc40f1435a5217aca15f97b
SitiveMC <stivie.marcu02@gmail.com>
2021-05-19 10:55:23 -0500

    FIX.INTERFAZ.CLIENTE

--- src/Normas.Web.Client/Models/NormaVM.cs
+++ src/Normas.Web.Client/Models/NormaVM.cs
@@ -11,8 +11,9 @@ namespace Normas.Web.Client.Models
         public String Descripcion { get; set; }
         
         [DataType(DataType.Date)]
-        [Display(Name = "Fecha Publicación")]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Fecha de Publicación")]
+        //[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime FechaPublicacion { get; set; }
         public Boolean Activo { get; set; }
         public int TipoRequisitoId { get; set; }
